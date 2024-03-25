7ef655a896f24a1264e762e3a95ed6f07c014370
robin0909 <robinyang_beijing@163.com>
2024-03-25 17:45:01 +0800

    feat: deploy contracts

--- scripts/deployPool.ts
+++ scripts/deployPool.ts
@@ -13,7 +13,7 @@ export async function run(provider: NetworkProvider) {
         },
         {
             $$type: 'Deploy',
-            queryId: 0n,
+            queryId: 1n,
         }
     );
 
