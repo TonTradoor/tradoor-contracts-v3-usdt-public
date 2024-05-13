13b2a5246f485727febbfb8f9bcced3540bc7acd
Fredy <fredy_xiang@163.com>
2024-05-13 12:05:18 +0800

    deploy 103 for devnet

--- blueprint.config.ts
+++ blueprint.config.ts
@@ -2,8 +2,8 @@ import { Config } from '@ton/blueprint';
 
 export const config: Config = {
     network: {
-        // endpoint: 'https://testnet.toncenter.com/api/v2',
-        endpoint: 'https://sandbox-v4.tonhubapi.com',
+        endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+        // endpoint: 'https://sandbox.tonhubapi.com/jsonRPC',
         type: 'testnet',
         version: 'v2',
         key: '6eb37dd6dd6cf27b74cf704b9dac04f653bf1daf2877b7b0e43f80226574193f',
