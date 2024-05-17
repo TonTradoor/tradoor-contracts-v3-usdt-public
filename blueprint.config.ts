27af022e3731ae58c9174c32987797baff4d4f4f
Fredy <fredy_xiang@163.com>
2024-05-17 20:35:41 +0800

    update custom network config

--- blueprint.config.ts
+++ blueprint.config.ts
@@ -2,10 +2,14 @@ import { Config } from '@ton/blueprint';
 
 export const config: Config = {
     network: {
-        endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
-        // endpoint: 'https://sandbox.tonhubapi.com/jsonRPC',
+        endpoint: 'https://sandbox-v4.tonhubapi.com',
         type: 'testnet',
-        version: 'v2',
-        key: '6eb37dd6dd6cf27b74cf704b9dac04f653bf1daf2877b7b0e43f80226574193f',
+        version: 'v4',
     },
+    // network: {
+    //     endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+    //     type: 'testnet',
+    //     version: 'v2',
+    //     key: '6eb37dd6dd6cf27b74cf704b9dac04f653bf1daf2877b7b0e43f80226574193f',
+    // },
 };
--- scripts/getData.ts
+++ scripts/getData.ts
@@ -3,9 +3,9 @@ import { attachOrderBook, attachPool, getConfig } from '../utils/util';
 import { Address } from '@ton/core';
 
 export async function run(provider: NetworkProvider) {
-    const account = Address.parse(await provider.ui().input('account address:'));
+    // const account = Address.parse(await provider.ui().input('account address:'));
     const tokenId = BigInt(await provider.ui().input('tokenId:'));
-    const orderId = BigInt(await provider.ui().input('orderId:'));
+    // const orderId = BigInt(await provider.ui().input('orderId:'));
 
     const orderBook = attachOrderBook(provider);
     const pool = attachPool(provider);
@@ -13,43 +13,43 @@ export async function run(provider: NetworkProvider) {
     // console.log('=================== Config ===================');
     // console.log('orderbook config:', await orderBook.getConfigData(account));
     // console.log('pool config:', await pool.getConfigData(account));
-    // console.log('token config:', await pool.getTokenConfig(tokenId));
+    console.log('token config:', await pool.getTokenConfig(tokenId));
 
-    console.log('=================== LP ===================');
-    // get index
-    let lpOrderIdNext = (await orderBook.getLpPositionOrder(0n)).lpPositionOrderIndexNext;
-    console.log(`lpOrderIdNext:`, lpOrderIdNext);
+    // console.log('=================== LP ===================');
+    // // get index
+    // let lpOrderIdNext = (await orderBook.getLpPositionOrder(0n)).lpPositionOrderIndexNext;
+    // console.log(`lpOrderIdNext:`, lpOrderIdNext);
 
-    if (orderId < 0) {
-        console.log('order not exist');
-        return;
-    }
+    // if (orderId < 0) {
+    //     console.log('order not exist');
+    //     return;
+    // }
 
-    // get order
-    let lpOrder = await orderBook.getLpPositionOrder(orderId);
-    console.log(`lpOrder:`, lpOrder);
+    // // get order
+    // let lpOrder = await orderBook.getLpPositionOrder(orderId);
+    // console.log(`lpOrder:`, lpOrder);
 
-    // get position
-    let lpPosition = await pool.getLpPosition(account);
-    console.log(`lpPosition:`, lpPosition);
+    // // get position
+    // let lpPosition = await pool.getLpPosition(account);
+    // console.log(`lpPosition:`, lpPosition);
 
-    console.log('=================== Perp ===================');
-    // get index
-    let orderIdNext = (await orderBook.getPerpPositionOrder(0n)).perpPositionOrderIndexNext;
-    console.log(`orderIdNext:`, orderIdNext);
+    // console.log('=================== Perp ===================');
+    // // get index
+    // let orderIdNext = (await orderBook.getPerpPositionOrder(0n)).perpPositionOrderIndexNext;
+    // console.log(`orderIdNext:`, orderIdNext);
 
-    // get order
-    let order = await orderBook.getPerpPositionOrder(orderId);
-    console.log(`order:`, order);
+    // // get order
+    // let order = await orderBook.getPerpPositionOrder(orderId);
+    // console.log(`order:`, order);
 
-    // get position
-    let position = await pool.getPerpPosition(tokenId, account);
-    console.log(`position:`, position);
+    // // get position
+    // let position = await pool.getPerpPosition(tokenId, account);
+    // console.log(`position:`, position);
 
-    // get price
-    console.log('price', await pool.getPriceData(tokenId));
+    // // get price
+    // console.log('price', await pool.getPriceData(tokenId));
 
-    let tokenConfig = await pool.getTokenConfig(tokenId);
-    console.log('tokenConfig:', tokenConfig);
+    // let tokenConfig = await pool.getTokenConfig(tokenId);
+    // console.log('tokenConfig:', tokenConfig);
 
 }
