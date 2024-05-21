755f587fb18efc203b39d8b6911db987e96947de
Fredy <fredy_xiang@163.com>
2024-05-21 18:03:50 +0800

    update transfer script

--- scripts/transferJetton.ts
+++ scripts/transferJetton.ts
@@ -1,4 +1,4 @@
-import { Address, beginCell, toNano } from '@ton/core';
+import { Address, beginCell, fromNano, toNano } from '@ton/core';
 import { NetworkProvider, sleep } from '@ton/blueprint';
 import { attachJettonWallet, attachMockJetton, fromUnits, getConfig, getLastTransaction, toUnits, waitForTransaction } from '../utils/util';
 import { JETTON_DECIMAL } from '../utils/constants';
@@ -7,9 +7,10 @@ export async function run(provider: NetworkProvider) {
     const sampleJetton = attachMockJetton(provider);
 
     const recevier = Address.parse(await provider.ui().input('recevier address:'));
-    const amount = await provider.ui().input('mint amount:');
+    const tonAmount = await provider.ui().input('ton amount:');
+    const jettonAmount = await provider.ui().input('jetton amount:');
 
-    console.log(`transfer to ${recevier} for ${amount}`);
+    console.log(`transfer to ${recevier} for ton ${tonAmount} and jetton ${jettonAmount}`);
 
     const lastTrx = await getLastTransaction(provider, sampleJetton.address);
 
@@ -21,16 +22,16 @@ export async function run(provider: NetworkProvider) {
     await senderJettonWallet.send(
         provider.sender(),
         {
-            value: toNano(0.2),
+            value: toNano(tonAmount + 0.2),
         },
         {
             $$type: 'TokenTransfer',
             query_id: 0n,
-            amount: toUnits(amount, JETTON_DECIMAL),
+            amount: toUnits(jettonAmount, JETTON_DECIMAL),
             sender: recevier,
             response_destination: provider.sender().address!!,
             custom_payload: null,
-            forward_ton_amount: toNano(0),
+            forward_ton_amount: toNano(tonAmount),
             forward_payload: beginCell().storeUint(0, 1).endCell()
         }
     );
