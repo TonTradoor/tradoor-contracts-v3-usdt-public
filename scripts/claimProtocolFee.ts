87ad25cfb77eb61ee0bbb5ed2ec9632c0c9fd485
Eric <verichenn@gmail.com>
2024-09-05 14:59:16 +0800

    fix: claim protocol fee.

---
+++ scripts/claimProtocolFee.ts
@@ -0,0 +1,35 @@
+import { Address, toNano } from '@ton/core';
+import { NetworkProvider } from '@ton/blueprint';
+import { attachPool, getLastTransaction, waitForTransaction } from '../utils/util';
+
+export async function run(provider: NetworkProvider) {
+    const feeReceiver = await provider.ui().input('feeReceiver:');
+
+    console.log("feeReceiver:", feeReceiver);
+    const sure = await provider.ui().choose('continue execution??????', ['no', 'yes'], a => a) === 'yes';
+    if (!sure) {
+        return;
+    }
+
+    const pool = attachPool(provider);
+
+    const lastTrx = await getLastTransaction(provider, pool.address);
+    await pool.send(
+        provider.sender(),
+        {
+            value: toNano('0.1')
+        },
+        {
+            $$type: 'ClaimProtocolFee',
+            feeReceiver: Address.parse(feeReceiver),
+            trxId: 0n,
+        }
+    );
+
+    // wait for trx
+    const transDone = await waitForTransaction(provider, pool.address, lastTrx, 20);
+    if (transDone) {
+        console.log(`claim protocol fee success`);
+    }
+
+}
