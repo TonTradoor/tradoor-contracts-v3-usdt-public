7fcbc604837298a51d16fe024f593fa68b3fc8a8
Eric <verichenn@gmail.com>
2024-09-07 21:37:55 +0800

    fix: jetton.

--- blueprint.config.ts
+++ blueprint.config.ts
@@ -5,17 +5,17 @@ console.log('use env', process.env.NODE_ENV);
 let config: Config;
 if (process.env.NODE_ENV === 'mainnet') {
     config = {
-        // network: {
-        //     endpoint: 'https://mainnet-v4.tonhubapi.com',
-        //     type: 'mainnet',
-        //     version: 'v4',
-        // },
         network: {
-            endpoint: 'https://toncenter.com/api/v2/jsonRPC',
+            endpoint: 'https://mainnet-v4.tonhubapi.com',
             type: 'mainnet',
-            version: 'v2',
-            key: 'd62949d66112cb5623f22889b5730eb8460eb5ed051f00ba06c179669705bf18'
+            version: 'v4',
         },
+        // network: {
+        //     endpoint: 'https://toncenter.com/api/v2/jsonRPC',
+        //     type: 'mainnet',
+        //     version: 'v2',
+        //     key: 'd62949d66112cb5623f22889b5730eb8460eb5ed051f00ba06c179669705bf18'
+        // },
     };
 } else {
     config = {
